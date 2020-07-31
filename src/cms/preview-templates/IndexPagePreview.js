0e9f9384734c2a2ed514c99cff3906f3a5e66278
Solomon Omojola <omojolasolomon@gmail.com>
2020-07-31 16:03:54 +0200

    update index

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -3,30 +3,35 @@ import PropTypes from 'prop-types'
 import { IndexPageTemplate } from '../../templates/index-page'
 
 const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
-
-  if (data) {
+    // const data = entry.getIn(['data']).toJS()
+    // if (data) {
+    //   return (
+    //     <IndexPageTemplate
+    //       image={getAsset(data.image)}
+    //       title={data.title}
+    //       heading={data.heading}
+    //       subheading={data.subheading}
+    //       description={data.description}
+    //       intro={data.intro || { blurbs: [] }}
+    //       mainpitch={data.mainpitch || {}}
+    //     />
+    //   )
+    // } else {
+    //   return <div>Loading...</div>
+    // }
     return (
-      <IndexPageTemplate
-        image={getAsset(data.image)}
-        title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-      />
+        <img
+            src="https://images.unsplash.com/photo-1538219008904-166ea576a586?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+            objectFit="cover"
+        />
     )
-  } else {
-    return <div>Loading...</div>
-  }
 }
 
 IndexPagePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  getAsset: PropTypes.func,
+    entry: PropTypes.shape({
+        getIn: PropTypes.func,
+    }),
+    getAsset: PropTypes.func,
 }
 
 export default IndexPagePreview
